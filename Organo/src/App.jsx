bf48dd42fc9c95d7f796096aeeb9fc1b3b2aec25
Gabriel Portes <152006861+GVPortes@users.noreply.github.com>
2025-04-10 11:48:16 -0300

    att

--- Organo/src/App.jsx
+++ Organo/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <div className='App'>
       <Banner />
       <Formulario />
-      </div>
+    </div>
   )
 }
 
--- Organo/src/componentes/Formulario/index.jsx
+++ Organo/src/componentes/Formulario/index.jsx
@@ -1,7 +1,20 @@
 import CampoTexto from "../CampoTexto";
+import ListaSupensa from "../ListaSuspensa";
 import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão",
+    "Teste e Qualidade",
+  ];
+
+
   return (
     <setion className="formulario">
       <form>
@@ -10,6 +23,7 @@ const Formulario = () => {
         <CampoTexto label="Nome" placeholder="Digite aqui seu nome" />
         <CampoTexto label="Cargo" placeholder="Digite aqui seu cargo" />
         <CampoTexto label="Imagem" placeholder="Digite aqui o enderço da imagem"/>
+        <ListaSupensa label="Time" itens={times}/>
       </form>
     </setion>
   );
